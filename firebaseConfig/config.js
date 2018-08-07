614d9a2dd48a6d5701b0a9cb3ae14f4e4c2c1c27
Thomas Johan Back <tjb295@nau.edu>
2018-08-06 21:42:09 -0700

    Delete config.js

--- firebaseConfig/config.js
+++
@@ -1,9 +0,0 @@
-export const config = {
-    apiKey: "AIzaSyDPPOMr4RR92-kq7UgLaOBmo4QEHRjm7kk",
-    authDomain: "reminderflashcards.firebaseapp.com",
-    databaseURL: "https://reminderflashcards.firebaseio.com",
-    projectId: "reminderflashcards",
-    storageBucket: "",
-    messagingSenderId: "214885081112"
-
-};
